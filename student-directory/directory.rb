cef28fc30d95a8c84515dab786d08bd080e80696
Robert <fishwickrob@live.com>
2018-04-12 18:23:36 +0000

    Arguments from command line

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -3,11 +3,11 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -53,10 +53,22 @@ end
 def load_students
   file = File.open("students.csv", "r")
   file.readlines.each do |line|
-  name, cohort = line.chomp.split(', ')
+  name, cohort = line.chomp.split(", ")
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+
+def try_load_students
+  file_name = ARGV.first
+  return if file_name.nil?
+  if File.exists?(file_name)
+    load_students(file_name)
+      puts "Loaded #{students.count} from #{file_name}"
+  else
+    puts "Sorry, #{file_name} doesn't exist"
+    exit
+  end
 end
 
 def process(selection)
@@ -79,8 +91,9 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
+try_load_students
 interactive_menu
